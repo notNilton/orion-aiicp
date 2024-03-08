2e02fe0f227c74cc2616f86c3c5b3bf0042df7bb
notNilton <nilton.naab@gmail.com>
2024-03-07 20:55:42 -0400

    Update Script.py

--- PythonScript/Script.py
+++ PythonScript/Script.py
@@ -17,79 +17,8 @@ n = 64
 originalImage = Image.open(imageLocation)
 reducedImage = originalImage.resize((n, n), Image.BILINEAR)
 pixelImage = reducedImage.resize(originalImage.size, Image.NEAREST)
-if os.path.exists("ai1.png"):
-  os.remove("ai1.png")
-  pixelImage.save("ai1.png")
+if os.path.exists(imageLocation):
+  os.remove(imageLocation)
+  pixelImage.save(imageLocation)
 else: 
-  pixelImage.save("ai1.png")
-
-
-# img = Image.open("./PythonScript/16bitImage.png")
-
-# img_arr = np.array(img)
-# # Print shape of array (dimensions of matrix)
-# print(img_arr.shape)
-
-# # Print first 5 rows and columns of array (first 5x5 pixels of image)
-# print(img_arr[:5, :5])
-
-# def get_image_palette(image_path, num_colors):
-#     # Open the image using Pillow
-#     image = Image.open(image_path)
-
-#     # Resize the image to a small size to speed up clustering
-#     image = image.resize((100, 100))
-
-#     # Convert the image to a numpy array
-#     image_array = np.array(image)
-
-#     # Reshape the array to a list of RGB values
-#     pixels = image_array.reshape((-1, 3))
-
-#     # Use KMeans clustering to find dominant colors
-#     kmeans = KMeans(n_clusters=num_colors)
-#     kmeans.fit(pixels)
-
-#     # Get the RGB values of the cluster centers
-#     dominant_colors = kmeans.cluster_centers_.astype(int)
-
-#     # Convert the dominant colors to a list of tuples
-#     dominant_colors = [tuple(color) for color in dominant_colors]
-
-#     return dominant_colors
-
-# # def display_color_palette(image_path, num_colors):
-# #     # Create a window
-# #     window = tk.Tk()
-# #     window.title("Color Palette Viewer")
-
-# #     # Get the dominant colors in the image
-# #     palette = get_image_palette(image_path, num_colors)
-
-# #     # Display the color palette
-# #     for i, color in enumerate(palette):
-# #         hex_color = "#{:02x}{:02x}{:02x}".format(*color)
-        
-# #         # Create a canvas to display the color
-# #         canvas = Canvas(window, width=50, height=50, bg=hex_color)
-# #         canvas.grid(row=0, column=i, padx=5, pady=5)
-
-# #         # Display the color in RGB format
-# #         label = Label(window, text=f"RGB: {color}", padx=5, pady=5)
-# #         label.grid(row=1, column=i)
-
-# #     # Run the application
-# #     window.mainloop()
-
-# if __name__ == "__main__":
-#     # Provide the path to the image file
-#     image_path = "./PythonScript/testImage1.webp"
-
-#     # Specify the number of colors you want in the palette
-#     num_colors = 5
-
-#     # Get the dominant colors in the image
-#     palette = get_image_palette(image_path, num_colors)
-
-#     # Print the palette
-#     print("Image Palette:", palette)
+  pixelImage.save(imageLocation)
